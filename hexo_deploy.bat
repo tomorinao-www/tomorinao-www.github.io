222b3b1d802f8a2d19a77adad696a6480cd1b255
友利奈绪-勿忘我 <1520872279@qq.com>
2024-10-23 02:00:04 +0800

    up

--- hexo_deploy.bat
+++ hexo_deploy.bat
@@ -1,4 +1,5 @@
 call hexo clean
 call hexo g --config _config.yml,_config.arknights.yml,_config_deploy.yml
+call withclash.bat
 call hexo deploy --config _config.yml,_config.arknights.yml,_config_deploy.yml
 pause

46b886ec450217ef8cabb6155b2608d9d85ee363
友利奈绪-勿忘我 <1520872279@qq.com>
2024-11-30 00:22:40 +0800

    up

--- d.bat
+++ d.bat
@@ -3,5 +3,5 @@ call hexo g
 call hexo deploy --config  _config.yml,_config.arknights.yml,.nao_config.yml
 echo put -r public/* | sftp  root@121.40.221.30:/var/www/hexo/public/
 echo put -r public/* | sftp  root@23.95.227.104:/var/www/hexo/public/
-call ssh root@23.95.227.104 "cd /var/www/tomorinao-www.github.io && git pull"
+call ssh root@23.95.227.104 "cd /var/www/tomorinao-www.github.io && git pull  && git checkout gh-pages && git pull"
 pause
---
+++ source/_posts/color.md
@@ -0,0 +1,16 @@
+---
+title: css - color - 调色板 - 水色 - 粉红色
+categories: 推荐
+tags:
+  - 书籍
+  - 推荐
+comments: true
+toc: true
+donate: true
+share: true
+date: 2024-11-30 00:07:21
+---
+
+# css - color - 调色板 - 水色 - 粉红色
+
+<iframe src="/html/color.html" width="100%" height="1200"></iframe>
